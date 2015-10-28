1ee4d6ef215bbeadc64af1a8af2d05faa6eb7811
Jozef Šumaj <374029@mail.muni.cz>
2015-10-28 13:01:45 +0100

    Added initial model for Item (Service + Type, Tire)

---
+++ PA165_pneuservis/src/main/java/cz/muni/fi/pa165_pneuservis/model/Item.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165_pneuservis.model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Jozef.Sumaj
+ */
+public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
---
+++ PA165_pneuservis/src/main/java/cz/muni/fi/pa165_pneuservis/model/Tire.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165_pneuservis.model;
+
+/**
+ *
+ * @author Jozef.Sumaj
+ */
+public class Tire extends Item {
+    private String brand;
+    private Integer width;
+    private Integer ratio;
+    private Integer rim;
+}
