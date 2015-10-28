017531fc13a9ee7c9d6a4bc17979394e88523e59
Jakub Holy <saintjackie@gmail.com>
2015-10-28 18:25:37 +0100

    add entity Order,OrderItem

---
+++ PA165_pneuservis/src/main/java/cz/muni/fi/pa165_pneuservis/model/Order.java
@@ -0,0 +1,99 @@
+package cz.muni.fi.pa165_pneuservis.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Jakub Holy
+ */
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private BigDecimal totalPrice;    
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date completeDate;
+    
+    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getCompleteDate() {
+        return completeDate;
+    }
+
+    public void setCompleteDate(Date completeDate) {
+        this.completeDate = completeDate;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return Collections.unmodifiableList(orderItems);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+    }
+    
+    @Override
+    public int hashCode(){
+        int result=3;
+        result = 47 * result + ((id == null) ? 0 : id.hashCode());
+        result = 47 * result + ((createDate == null) ? 0 : createDate.hashCode());
+        result = 47 * result + ((totalPrice == null) ? 0 : totalPrice.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        if(o == this) { 
+            return true;
+        }
+        if((o == null) || (getClass() != o.getClass())){
+            return false;
+        }
+        
+        Order otherObject = (Order)o;
+        
+        if(!id.equals(otherObject.id)){
+            return false;
+        }
+        if(!createDate.equals(otherObject.createDate)){
+            return false;
+        }
+        return true;
+    }
+}
---
+++ PA165_pneuservis/src/main/java/cz/muni/fi/pa165_pneuservis/model/OrderItem.java
@@ -0,0 +1,72 @@
+package cz.muni.fi.pa165_pneuservis.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Jakub Holy
+ */
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private Item item;
+    
+    private Integer amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = 3;
+        result = 41 * result + ((id == null)? 0 : id.hashCode());
+        result = 41 * result + ((item == null)? 0 : item.hashCode());
+        result = 41 * result + ((amount == null)? 0 : amount.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        if(o == this) { 
+            return true;
+        }
+        if((o == null) || (getClass() != o.getClass())){
+            return false;
+        }
+        OrderItem otherObject = (OrderItem)o;
+        
+        if(!id.equals(otherObject.id)){
+            return false;
+        }
+        if(!amount.equals(otherObject.amount)){
+            return false;
+        }
+        if(!item.equals(otherObject.item)){
+            return false;
+        }
+        return true;
+    }
+}
