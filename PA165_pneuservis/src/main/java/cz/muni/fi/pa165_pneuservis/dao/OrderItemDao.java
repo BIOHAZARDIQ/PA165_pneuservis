fc635f197ea1e8b4f8d1b92b46980cd0ab5dd590
Jakub Holy <saintjackie@gmail.com>
2015-10-28 21:16:54 +0100

    Dao for Order,OrderItem added

---
+++ PA165_pneuservis/src/main/java/cz/muni/fi/pa165_pneuservis/dao/OrderItemDao.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165_pneuservis.dao;
+
+import cz.muni.fi.pa165_pneuservis.model.OrderItem;
+import java.util.List;
+
+/**
+ *
+ * @author Jakub Holy
+ */
+public interface OrderItemDao {
+    
+    public void create(OrderItem orderItem);
+    public OrderItem update(OrderItem orderItem);
+    public void remove(OrderItem orderItem);
+    public OrderItem findById(Long id);
+    public List<OrderItem> findAll();
+}
