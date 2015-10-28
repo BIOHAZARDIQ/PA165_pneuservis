f7a085efa9d6725c33359cf46c7632946edfee23
Ondrej Komarek <448288@mail.muni.cz>
2015-10-28 16:30:45 +0100

    added dependencies

--- PA165_pneuservis/src/main/java/cz/muni/fi/pa165_pneuservis/Main.java
+++ PA165_pneuservis/src/main/java/cz/muni/fi/pa165_pneuservis/Main.java
@@ -1,11 +1,20 @@
 
 package cz.muni.fi.pa165_pneuservis;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  *
  * @author Filip Meszaros <436321@mail.muni.cz>
  */
 public class Main {
+    
+    private static EntityManagerFactory entityManagerFactory;
+    
     public static void main(String[] args) {
         System.out.println("Hello PA165 project!");
     }
